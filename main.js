d503601a160fed2b54517c4b70000177edf7e2b6
Wesley Bernardes <bwesleysouza@gmail.com>
2021-07-20 04:36:12 -0300

    Finished the 5th day

--- main.js
+++ main.js
@@ -47,16 +47,18 @@ scrollReveal.reveal(
     , { interval: 100 }
 );
 
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
 function changeHeaderWhenScroll() {
-    const header = document.querySelector("#header");
-    const navHeight = header.offsetHeight;
 
     if(window.scrollY >= navHeight) return header.classList.add("scroll")
     else return header.classList.remove("scroll");
 }
 
+const backToTopButton = document.querySelector(".back-to-top");
+
 function backToTop() {
-    const backToTopButton = document.querySelector(".back-to-top");
 
     if(window.scrollY >= 500) {
         backToTopButton.classList.add("show");
@@ -65,7 +67,29 @@ function backToTop() {
     }
 }
 
+const sections = document.querySelectorAll("main section[id");
+
+function activateMenuCurrentSection() {
+    const boundary = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+    for(const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute("id");
+
+        const boundaryStart = boundary >= sectionTop;
+        const boundaryEnd = boundary <= sectionTop + sectionHeight;
+
+        if(boundaryStart && boundaryEnd) {
+            document.querySelector(`nav ul li a[href*=${sectionId}]`).classList.add("active");
+        } else {
+            document.querySelector(`nav ul li a[href*=${sectionId}]`).classList.remove("active");
+        }
+    }
+}
+
 window.addEventListener("scroll", () => {
     changeHeaderWhenScroll();
     backToTop();
+    activateMenuCurrentSection();
 })
